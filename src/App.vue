20ffcafb4d9922035765f33a2182440766ff221e
chuluq <chuluq@arvis.id>
2024-12-12 21:02:24 +0700

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+import Balance from './components/Balance.vue'
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue'
+</script>
+
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+<script lang="ts">
+export default {
+  name: 'HeaderItem',
+}
+</script>
+
+<template>
+  <h2>Expense Tracker</h2>
+</template>
