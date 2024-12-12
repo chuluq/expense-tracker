44f59f4469c7ec3b51cc196714a6830f2d3a03f6
chuluq <chuluq@arvis.id>
2024-12-12 21:20:41 +0700

    chore: use dummy data of transactions

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,7 +1,26 @@
+<script setup lang="ts">
+interface Transaction {
+  id: number
+  text: string
+  amount: number
+}
+
+const transactions: Transaction[] = [
+  { id: 1, text: 'Flower', amount: -19.99 },
+  { id: 2, text: 'Salary', amount: 299.97 },
+]
+</script>
+
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">Cash <span>-$400</span><button class="delete-btn">x</button></li>
-    <li class="plus">Paycheck <span>$800</span><button class="delete-btn">x</button></li>
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span>
+      <button class="delete-btn">x</button>
+    </li>
   </ul>
 </template>
